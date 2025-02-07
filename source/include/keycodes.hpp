99f446203f90859bd2977ea659e45f3cf07cbb56
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2025-02-07 12:47:36 -0500

    Update keycodes.hpp

--- source/include/keycodes.hpp
+++ source/include/keycodes.hpp
@@ -1,9 +1,13 @@
 // keycodes declared here as title suggests
 #ifndef KEYCODES_H
 #define KEYCODES_H
+#define KEYCODES
 
 // include c-int
+#include <iostream>
 #include <cstdint>
+#include <string>
+#include <map>
 
 // uint8 keycodes
 uint8_t KEY_A = 0x01;
@@ -33,5 +37,20 @@ uint32_t KEY_CPAD_RIGHT = 0x10000000;
 uint32_t KEY_CPAD_LEFT = 0x20000000;
 uint32_t KEY_CPAD_UP = 0x40000000;
 uint32_t KEY_CPAD_DOWN = 0x80000000;
+
+// static pointers to address'
+uintptr_t ADDRESS_0 = 0x1F89E8; // add0 and add9 do not follow pattern found in add1-8 (self comment)
+uintptr_t ADDRESS_1 = 0x1F8948;
+uintptr_t ADDRESS_2 = 0x1F8A00;
+uintptr_t ADDRESS_3 = 0x1F8A18;
+uintptr_t ADDRESS_4 = 0x1F8A30;
+uintptr_t ADDRESS_5 = 0x1F8A48;
+uintptr_t ADDRESS_6 = 0x1F8A60;
+uintptr_t ADDRESS_7 = 0x1F8A78;
+uintptr_t ADDRESS_8 = 0x1F8A90;
+uintptr_t ADDRESS_9 = 0x1F8AB0;
+
+// executable size (base address)
+uint64_t ADDRESS_BASE = 0x93A000;
 
 #endif
