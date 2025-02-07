b8af6411a1e77232aaba6a8a99097c31ea239c98
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2025-02-07 11:54:30 -0500

    Add files via upload

---
+++ source/include/keycodes.hpp
@@ -0,0 +1,37 @@
+// keycodes declared here as title suggests
+#ifndef KEYCODES_H
+#define KEYCODES_H
+
+// include c-int
+#include <cstdint>
+
+// uint8 keycodes
+uint8_t KEY_A = 0x01;
+uint8_t KEY_B = 0x02;
+uint8_t KEY_SELECT = 0x4;
+uint8_t KEY_START = 0x8;
+uint8_t KEY_RIGHT = 0x10;
+uint8_t KEY_LEFT = 0x20;
+uint8_t KEY_UP = 0x40;
+uint8_t KEY_DOWN = 0x80;
+
+// uint16 keycodes
+uint16_t KEY_R = 0x100;
+uint16_t KEY_L = 0x200;
+uint16_t KEY_X = 0x400;
+uint16_t KEY_Y = 0x800;
+uint16_t KEY_ZL = 0x4000;
+uint16_t KEY_ZR = 0x8000;
+
+// uint32 keycodes
+uint32_t KEY_TOUCH = 0x100000;
+uint32_t KEY_CSTICK_RIGHT = 0x1000000;
+uint32_t KEY_CSTICK_LEFT = 0x2000000;
+uint32_t KEY_CSTICK_UP = 0x4000000;
+uint32_t KEY_CSTICK_DOWN = 0x8000000;
+uint32_t KEY_CPAD_RIGHT = 0x10000000;
+uint32_t KEY_CPAD_LEFT = 0x20000000;
+uint32_t KEY_CPAD_UP = 0x40000000;
+uint32_t KEY_CPAD_DOWN = 0x80000000;
+
+#endif
